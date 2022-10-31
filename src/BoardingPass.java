8981832c3ad8776269f5c5ac2fffa196e5cbfe83
Ben Kores <benkores@gmail.com>
2022-10-31 01:04:24 -0400

    Boarding Pass bugfix

--- src/BoardingPass.java
+++ src/BoardingPass.java
@@ -17,14 +17,14 @@ public class BoardingPass {
 			System.out.println("Flight ID: " + bookings.get(i).get(0));
 			System.out.println("From: " + bookings.get(i).get(1));
 			System.out.println("To: " + bookings.get(i).get(2));
-			System.out.println("Passenger Name: " + bookings.get(i).get(3));
-			System.out.println("Person Type: " + bookings.get(i).get(4));
-			System.out.println("Gate: " + bookings.get(i).get(5));
-			System.out.println("Terminal: " + bookings.get(i).get(6));
-			System.out.println("Boarding begins: " + bookings.get(i).get(7));
-			System.out.println("Boarding ends: " + bookings.get(i).get(8));
-			System.out.println("Boarding group: " + bookings.get(i).get(9));
-			System.out.println("Seat: " + bookings.get(i).get(10));
+			System.out.println("Passenger Name: " + bookings.get(i).get(3) + " " + bookings.get(i).get(4));
+			System.out.println("Person Type: " + bookings.get(i).get(5));
+			System.out.println("Gate: " + bookings.get(i).get(6));
+			System.out.println("Terminal: " + bookings.get(i).get(7));
+			System.out.println("Boarding begins: " + bookings.get(i).get(8));
+			System.out.println("Boarding ends: " + bookings.get(i).get(9));
+			System.out.println("Boarding group: " + bookings.get(i).get(10));
+			System.out.println("Seat: " + bookings.get(i).get(11));
 			System.out.println("----------------------------------");
 			i++;
 		}
